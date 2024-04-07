7b1065967f99524fa15c631e9f864da806c504b5
cryptodev <fullstackagency123@gmail.com>
2024-04-07 15:34:19 -0500

    frontend controller modified

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2166,7 +2166,7 @@ class FrontendController extends Controller
             }
         }
         // for user mail
-        $ticket_book = NotificationTemplate::where('title', 'Book Ticket')->first();
+        $ticket_book = NotificationTemplate::where('title', 'Order Notification')->first();
         $details['user_name'] = $user->name . ' ' . $user->last_name;
         $details['quantity'] = $request['quantity'];
         $details['event_name'] = Event::find($order->event_id)->name;
